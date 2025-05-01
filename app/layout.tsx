2688b29967883e56adbc8e24cfaa12b1185d8a27
VilmaoWebServices <webservices00@hotmail.com>
2025-05-01 10:48:37 +0200

    vilmilabs_proto/00-07

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import {
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
